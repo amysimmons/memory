8bf1c1ffe410807a9e91086b055b2eff1de53d5d
Amy Simmons <amy__simmons@hotmail.com>
2016-03-24 20:22:21 +1100

    tiles flipping and unflipping

--- scripts/memory.js
+++ scripts/memory.js
@@ -6,6 +6,7 @@ Game = {
     Game.board = Game.generateBoard(Game.difficulty);
     Game.pairs = [];
     Game.matches = [];
+    Game.initEvents();
   },
 
   generateBoard: function(difficulty, values){
@@ -34,7 +35,8 @@ Game = {
         var tile = {
           position: [i, j],
           value: Game.getRandomValue(Game.values),
-          flipped: false
+          flipped: false,
+          matched: false
         }
         row.push(tile);
       };
@@ -60,8 +62,10 @@ Game = {
       for (var j = 0; j < grid[i].length; j++) {
         var tile = document.createElement('div');
         tile.className = grid[i][j].flipped ? 'tile flipped' : 'tile unflipped';
+        tile.xPos = j;
+        tile.yPos = i;
         var value = document.createTextNode(grid[i][j].value);
-        tile.appendChild(value)
+        tile.appendChild(value);
         row.appendChild(tile);
       };
 
@@ -71,18 +75,40 @@ Game = {
   },
 
   duplicateValues: function(values){
+
     return values.concat(values);
+
   },
 
   getRandomValue: function(values){
+
     var allValues = Game.duplicateValues(values);
     var shuffled = _.shuffle(allValues);
     return shuffled.pop();
+
+  },
+
+  initEvents: function(){
+
+    $('body').on('click', '.tile', function(event) {
+
+      if(Game.board[event.target.yPos][event.target.xPos].matched){
+        alert("You've already matched this tile");
+      }
+      else if(event.target.classList.contains("unflipped")){
+        Game.board[event.target.yPos][event.target.xPos].flipped = true;
+        event.target.className = "tile flipped";
+      }else{
+        Game.board[event.target.yPos][event.target.xPos].flipped = false;
+        event.target.className = "tile unflipped";
+      }
+
+    });
+
   }
 
 }
 
 $(document).ready(function(){
   Game.initialize();
-
 });
